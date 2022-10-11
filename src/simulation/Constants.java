1110ce52a1eae379b384b0ea90e170c7bc36915f
José Julián Brenes Garro <103540253+JoseJTDFan@users.noreply.github.com>
2022-10-10 23:51:06 -0600

    Update Constants.java

--- src/simulation/Constants.java
+++ src/simulation/Constants.java
@@ -3,14 +3,6 @@ package simulation;
 public interface Constants {
 	public static final int DAYS_IN_YEARS = 365;
 	public static final int DAY_IN_MILLIS = 500;
-	public static final int CHECKIN_TIME = DAY_IN_MILLIS;
 	public static final int ABONAR = 10;
 	public static final int REGAR = 10;
-	
-	/*
-	public static String UPDATE_DAYS = "DAYS";
-	public static String UPDATE_WEATHER = "WEATHER";
-	
-	public static final int THREAD_POOL_SIZE = 5;
-	*/
 }
